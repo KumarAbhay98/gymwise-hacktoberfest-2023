f1e67d4643357ba576dca559c722d71df0a0bb90
Abhay <Abhaycbr07@gmail.com>
2023-10-13 23:34:04 +0530

    commit

---
+++ data/blogs.js
@@ -0,0 +1,111 @@
+blogs = [
+    {
+        title: "How to Build the Habit of Exercising",
+        src: "https://th.bing.com/th/id/OIP.8nYaHEzHOviVnqmGq_ANVgHaLH?pid=ImgDet&rs=1",
+        excerpt: "Learn effective strategies to build a consistent exercise routine and achieve your fitness goals.",
+        content: `
+        <p><strong>How to Build the Habit of Exercising</strong></p>
+        <p>Exercise is essential for a healthy lifestyle, but establishing a consistent workout routine can be challenging. In this blog post, we'll explore effective strategies to help you build the habit of exercising.</p>
+        <p><strong>Introduction:</strong> Building the habit of exercising is crucial for maintaining a healthy lifestyle. We often start with enthusiasm but struggle to stick with it. However, with the right approach, exercise can become a natural part of your daily routine.</p>
+        <ol>
+          <li><strong>Set Realistic Goals:</strong> Start with small, manageable targets, and gradually increase the intensity and duration of your workouts. Realistic goals make it easier to stay committed.</li>
+          <li><strong>Create a Consistent Schedule:</strong> Consistency is key to forming a habit. Choose specific times each day or certain days of the week for your workouts. Treat these appointments with the same importance as any other commitments in your life.</li>
+          <li><strong>Find Activities You Enjoy:</strong> Exercise doesn't have to be boring. Discover physical activities that you genuinely enjoy, whether it's dancing, hiking, swimming, or playing sports. Having fun while working out increases your chances of sticking to your routine.</li>
+          <li><strong>Stay Accountable:</strong> Share your fitness goals with a friend or family member who can help keep you accountable. Having a workout buddy can provide motivation and encouragement. You can also track your progress using a fitness app or journal to celebrate your achievements.</li>
+        </ol>
+        <p><strong>Conclusion:</strong> Building the habit of exercising is a journey that requires dedication and patience. By setting realistic goals, creating a consistent schedule, finding enjoyable activities, and staying accountable, you can transform exercise into an integral part of your daily life. With each workout, you're taking a significant step toward a healthier and happier you. Start today and embrace the benefits of a regular exercise routine. Your body and mind will thank you.</p>`,
+        author: "Abhay Kumar",
+        date: "September 1, 2023",
+        slug: "build-exercise-habit",
+    },
+    {
+        title: "How to be fit",
+        src: "https://www.medicineandnutrition.org/wp-content/uploads/2020/07/Top-7-Home-Exercises-to-Get-Fit-Fast100.jpeg",
+        excerpt: "Discover a selection of the most effective exercises to improve your overall fitness and strength.",
+        content: `
+            <p>Getting fit requires a well-rounded exercise routine. In this blog post, we'll introduce you to some of the best exercises that can help you achieve your fitness goals. From cardiovascular workouts to strength training exercises, you'll find a variety of options to suit your preferences and needs.</p>
+            <p><strong>Cardiovascular Workouts:</strong></p>
+            <p>Cardio exercises are great for improving your heart health and burning calories. Activities like running, cycling, and swimming can get your heart rate up and boost your endurance. Aim for at least 150 minutes of moderate-intensity cardio each week.</p>
+            <hr>
+            <p><strong>Strength Training:</strong></p>
+            <p>Strength training exercises, such as weightlifting and bodyweight exercises, help build muscle mass and increase metabolism. Incorporate strength training into your routine at least two days a week, targeting different muscle groups.</p>
+            <hr>
+            <p><strong>Flexibility and Stretching:</strong></p>
+            <p>Don't forget about flexibility! Stretching exercises like yoga and Pilates can improve your range of motion and reduce the risk of injuries. Try to stretch for 10-15 minutes every day.</p>
+            <p><strong>Healthy Eating:</strong></p>
+            <p>Exercise alone isn't enough. Pair your workouts with a balanced diet. Focus on whole foods, lean proteins, fruits, vegetables, and stay hydrated. Proper nutrition is essential for your fitness journey.</p>
+            <hr>
+            <p><strong>Rest and Recovery:</strong></p>
+            <p>Rest is crucial for muscle recovery. Ensure you get 7-9 hours of quality sleep each night. Additionally, allow your muscles to recover by having rest days in your workout schedule.</p>
+            <p><strong>Conclusion:</strong></p>
+            <p>Building a fitness routine takes time and dedication. Start with small steps, gradually increase the intensity, and stay consistent. Remember that it's not just about looking good; it's about feeling healthy and energetic in your everyday life.</p>
+            `,
+        author: "Abhay Kumar",
+        date: "September 2, 2023",
+        slug: "best-fitness-exercises",
+    },
+    {
+        title: "How to Build Strong Abs",
+        src: "https://www.bodybuilding.com/images/2017/august/6-ways-to-get-ripped-six-pack-abs-header-v2-960x540.jpg",
+        excerpt: "Discover effective exercises and tips to build strong and defined abdominal muscles.",
+        content: `
+            <p>If you've always wanted strong, defined abs, you're in the right place. Building a solid core not only looks great but also contributes to overall strength and stability. In this blog post, we'll guide you through the process of achieving impressive abs.</p>
+            <p><strong>1. Core Exercises:</strong></p>
+            <p>The key to strong abs is targeted core exercises. Include moves like crunches, leg raises, planks, and Russian twists in your routine. Aim to work your core 2-3 times a week.</p>
+            <hr>
+            <p><strong>2. Cardiovascular Workouts:</strong></p>
+            <p>Cardio exercises help burn the layer of fat covering your abs. Incorporate activities like running, cycling, and HIIT workouts to boost fat loss and reveal your abdominal muscles.</p>
+            <hr>
+            <p><strong>3. Balanced Diet:</strong></p>
+            <p>Building abs requires a low body fat percentage. Focus on a balanced diet that includes lean proteins, whole grains, and plenty of vegetables. Avoid excessive sugar and processed foods.</p>
+            <hr>
+            <p><strong>4. Hydration:</strong></p>
+            <p>Staying hydrated is essential for muscle definition. Drink plenty of water throughout the day to support your workouts and maintain a lean physique.</p>
+            <hr>
+            <p><strong>5. Consistency:</strong></p>
+            <p>Consistency is key. Stick to your ab workout routine and diet plan. Results may take time, but with persistence, you'll see progress.</p>
+            <p><strong>Conclusion:</strong></p>
+            <p>Building strong abs requires dedication and a multifaceted approach. Combine targeted exercises, cardiovascular workouts, a balanced diet, and consistency to achieve the chiseled abs you desire.</p>
+        `,
+        author: "DHRUV TYAGI",
+        date: "September 03, 2023",
+        slug: "how-to-build-strong-abs",
+    },
+    {
+        title: "The Power of Mindfulness: Achieving Balance and Well-being",
+        src: "https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg",
+        excerpt: "Explore the profound benefits of mindfulness and learn how to incorporate it into your daily life to achieve balance and well-being.",
+        content: `
+            <p>In the fast-paced and often chaotic world we live in, finding a sense of balance and well-being can be a challenge. But what if there was a simple yet powerful tool that could help you navigate the complexities of life with grace and poise? This tool is mindfulness, and in this blog post, we will delve into its profound benefits and how to make it a part of your daily life.</p>
+            <p><strong>Understanding Mindfulness</strong></p>
+            <p>Mindfulness is the practice of being fully present in the moment, without judgment. It involves paying deliberate and non-reactive attention to your thoughts, feelings, and surroundings. By doing so, you can develop a greater awareness of yourself and your experiences.</p>
+            <p><strong>The Benefits of Mindfulness</strong></p>
+            <p>Mindfulness offers a wide range of benefits for your mental, emotional, and physical well-being:</p>
+            <ul>
+              <li><strong>Stress Reduction:</strong> Mindfulness can help you manage stress by teaching you to respond to challenges with calm and clarity.</li>
+              <li><strong>Improved Focus:</strong> It enhances your ability to concentrate and stay on task, improving productivity.</li>
+              <li><strong>Emotional Regulation:</strong> Mindfulness empowers you to better understand and manage your emotions, leading to more stable and satisfying relationships.</li>
+              <li><strong>Enhanced Resilience:</strong> It can help you bounce back from adversity and setbacks with greater ease.</li>
+              <li><strong>Physical Health:</strong> Mindfulness has been linked to improved immune function, reduced blood pressure, and better overall health.</li>
+            </ul>
+            <p><strong>Practical Tips for Cultivating Mindfulness</strong></p>
+            <p>Now that you understand the benefits, here are some practical tips to incorporate mindfulness into your life:</p>
+            <ul>
+              <li><strong>Meditation:</strong> Set aside a few minutes each day to meditate. Focus on your breath and let go of racing thoughts.</li>
+              <li><strong>Body Scan:</strong> Periodically scan your body for tension. Release any areas of tightness and breathe into them.</li>
+              <li><strong>Mindful Eating:</strong> Savor each bite of your meals, paying attention to taste, texture, and aroma.</li>
+              <li><strong>Engage Your Senses:</strong> When you walk, sit, or interact with others, engage your senses fully. Notice the details in your environment.</li>
+              <li><strong>Journaling:</strong> Keep a mindfulness journal to reflect on your experiences and track your progress.</li>
+            </ul>
+            <p><strong>Conclusion</strong></p>
+            <p>Mindfulness is a valuable tool for achieving balance and well-being in a hectic world. By integrating mindfulness practices into your daily life, you can reduce stress, enhance focus, and improve your overall quality of life. Start today and experience the transformative power of mindfulness for yourself.</p>
+        `,
+        author: "Dhruv Tyagi",
+        date: "October 13, 2023",
+        slug: "the-power-of-mindfulness"
+    },
+    // Add your new blog here.
+    
+// Paste your template here
+];
+module.exports = blogs;
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const exphbs = require('express-handlebars');
+const path = require('path')
+const dotenv = require('dotenv');
+const app = express()
+
+// below tow line of code is for handlebars you can simply visit express-handlebars and open its github
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+dotenv.config({ path: path.join(__dirname, 'config.env') });
+
+const PORT = process.env.PORT || 8080;
+
+app.set('views', path.join(__dirname, 'views'))
+//here we are going to use routes now
+app.use('/', require(path.join(__dirname, './routes/app.js')))
+
+app.listen(PORT,()=>{
+    console.log(`the app is running at http://localhost:${PORT}`)
+})
